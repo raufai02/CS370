0fee2f2d1f154af3874ef0847a4fd07ce49c2fef
pvloiselle <124627176+pvloiselle@users.noreply.github.com>
2023-03-27 15:29:36 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx';
+import Sign_In from './components/Sign_In/Sign_In.jsx';
+import Donation_Poster from "./components/Donor/Donation_Poster.jsx";
+//import Volunteer_Sign_Up from './components/Volunteer_Sign_Up/Volunteer_Sign_Up.jsx';
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Donor_Timeline from "./components/Donor/Donor_Timeline.jsx";
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" crossorigin></script>
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route path='/about' element={<About/>}  />
+        <Route path='/signin' element={<Sign_In/>}  />
+        <Route path='/donortimeline' element={<Donor_Timeline/>}  />
+        <Route path='/donationposter' element={<Donation_Poster/>}  />
+      </Routes>
+    </BrowserRouter> 
+  )
+}
+
+export default App;
---
+++ Home.jsx
@@ -0,0 +1,20 @@
+import Header from '../Header/Header.jsx';
+import './Home.css';
+
+export default function Home(){
+    return(
+        <body className="home">  
+            <Header num={1}/>
+            <section id="section0" className="container-fluid">
+                <div className="container">
+                    <div className="row">
+                        <div className="col align-self-center">
+                            <h2>WELCOME TO PROJECT MEALSWIPES </h2>
+                            <p>We are the solution to the 119 billion pounds of annual food waste in the US.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </body>
+    )
+}
