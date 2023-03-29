3700193a695d9f9e7deed53f27fa66bafeaca059
pvloiselle <124627176+pvloiselle@users.noreply.github.com>
2023-03-29 13:14:18 -0400

    Update Sign_In.jsx

--- Sign_In.jsx
+++ Sign_In.jsx
@@ -1,18 +1,88 @@
+//import Header from '../Header/Header.jsx';
+//import './Sign_In.css';
+//import {Link} from "react-router-dom";
+import volunteer from './volunteer.jpg';
+//import { useNavigate } from 'react-router-dom';
+//import { Button } from 'react-native';
+
 import Header from '../Header/Header.jsx';
 import './Sign_In.css';
 import {Link} from "react-router-dom";
-import volunteer from './volunteer.jpg';
-import { useNavigate } from 'react-router-dom';
-//import { Button } from 'react-native';
+//import volunteer from './volunteer.png';
+import React, {useState, useEffect} from 'react';
+import { auth } from "../../firebase";
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {db} from "../../firebase";
+import {doc, setDoc, getDoc} from "firebase/firestore";
+import {useNavigate} from 'react-router-dom';
 
 
 
 export default function Sign_In(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const navigate = useNavigate();
-    const handleSubmit = event => {
-        navigate("/donortimeline");
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential);
+            handleSubmit();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
+
+    const [authUser, setAuthUser] = useState(null);
+    const [role, setRole] = useState('');
+
+    //use the role variable to navigate to correct user page
+
+    useEffect(() =>{
+        const listen = onAuthStateChanged(auth, (user) =>{
+            if(user) {
+                userRole(user).then((rl) =>{
+                    setRole(rl)
+                    setAuthUser(user)
+                });
+            }else {
+                setAuthUser(null);
+                setRole(null);
+            }
+        });
+
+        return () =>{
+            listen();
+        }
+    }, []);
+
+    async function userRole(authUser){
+        const docRef = doc(db, "users", authUser.uid);
+        const docSnap = await getDoc(docRef);
+        return String(docSnap.data().role);
+
+    }
+
+    // This function "handleSubmit" allows us to submit data and link to a new page
+    const navigate = useNavigate();
+
+    const handleSubmit = event => {
+
+    // Redirect to volunteer ui
+    
+    if(role=="volunteer") {
+        navigate('/donorui');
+    }
+    if(role=="donor") {
+        navigate('/donorui');
+    }
+    
+    };
+
+
+
 
 
     return(
@@ -24,11 +94,11 @@ export default function Sign_In(){
                     <img src={volunteer} alt={"avatar"} className={"avatar"} />
                 </div>
                 <div className="signin_container">
-                    <form id="signin" onSubmit={handleSubmit}>
+                    <form id="signin" onSubmit={signIn}>
                         <label htmlFor="uname"><b>Username</b></label>
-                        <input id="siusername" type="text" placeholder="Enter Username" name="uname" required />
+                        <input id="email" type="email" placeholder="Enter Email" name="email" required value={email} onChange={(e)=>setEmail(e.target.value)}/>
                         <label htmlFor="psw"><b>Password</b></label>
-                        <input id="sipass" type="password" placeholder="Enter Password" name="psw" required />
+                        <input id="sipass" type="password" placeholder="Enter Password" name="psw" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
                         <button type="submit">Login</button>
                     </form>
                     <label>
@@ -37,7 +107,7 @@ export default function Sign_In(){
                 </div>
                 <div className="container">
                 <span className="psw">FORGOT <a className="forgotpass" href="#">PASSWORD?</a></span>
-                <p>NOT A MEMBER? SIGN UP <Link to='/volsignup'>HERE</Link> </p>
+                <p>NOT A MEMBER? SIGN UP <Link to='/onboarding'>HERE</Link> </p>
                 </div>
             </section>
         </body>
